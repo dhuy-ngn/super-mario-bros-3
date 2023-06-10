602505659fc385de9ad98f6868354e19c8af3433
Huy Nguyen <dhuy.ngn@gmail.com>
2023-06-10 21:04:28 +0700

    update naming convention and added update and bounding box for firetrap

--- super-mario-bros-3/FireTrap.cpp
+++ super-mario-bros-3/FireTrap.cpp
@@ -1,1 +1,20 @@
 #include "FireTrap.h"
+
+
+void CFireTrap::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+	left = x - FIRETRAP_BBOX_WIDTH / 2;
+	top = y - FIRETRAP_BBOX_HEIGHT / 2;
+	right = left + FIRETRAP_BBOX_WIDTH;
+	bottom = top + FIRETRAP_BBOX_HEIGHT;
+}
+
+void CFireTrap::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	CGameObject::Update(dt);
+	y += vy * dt;
+}
+
+void CFireTrap::Render()
+{
+}
