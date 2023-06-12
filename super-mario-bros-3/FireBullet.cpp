4ab2199eb4d590924232391818250cbc1cce083f
Huy Nguyen <dhuy.ngn@gmail.com>
2023-06-12 10:00:07 +0700

    added render and update function to firebullet

---
+++ super-mario-bros-3/FireBullet.cpp
@@ -0,0 +1,53 @@
+#include "FireBullet.h"
+#include "FireTrap.h"
+#include "Mario.h"
+
+FireBullet::FireBullet(float bx, float by, bool up, bool right) {
+	if (up) {
+		y = by;
+		vy = -BULLET_SPEED_Y;
+	}
+	else {
+		y = by;
+		vy = BULLET_SPEED_Y;
+	}
+
+	if (right)
+	{
+		x = bx;
+		vx = BULLET_SPEED_X;
+	}
+	else
+	{
+		x = bx;
+		vx = -BULLET_SPEED_X;
+	}
+}
+
+void FireBullet::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
+	CGameObject::Update(dt, coObjects);
+
+	x += vx * dt;
+	y += vy * dt;
+}
+
+void FireBullet::Render()
+{
+	CAnimations* animations = CAnimations::GetInstance();
+
+	int aniId = BULLET_ANI_RIGHT;
+	if (vx > 0)
+		aniId = BULLET_ANI_RIGHT;
+	else
+		aniId = BULLET_ANI_LEFT;
+
+	animations->Get(aniId)->Render(x, y);
+}
+
+void FireBullet::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - BULLET_BBOX_WIDTH / 2;
+	t = y - BULLET_BBOX_HEIGHT / 2;
+	r = l + BULLET_BBOX_WIDTH;
+	b = t + BULLET_BBOX_HEIGHT;
+}
