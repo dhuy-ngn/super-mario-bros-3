b087dde8854f0504df58890b05a35951e9d2b46d
Huy Nguyen <dhuy.ngn@gmail.com>
2023-06-12 15:43:58 +0700

    change color block to be inherited from CPlatform

--- super-mario-bros-3/ColorBlock.h
+++ super-mario-bros-3/ColorBlock.h
@@ -1,27 +1,17 @@
 #pragma once
 
-#include "GameObject.h"
+#include "Platform.h"
 
-class CColorBlock : public CGameObject
+class CColorBlock : public CPlatform
 {
-protected: 
-	int length;				// Unit: cell 
-	float cellWidth;
-	float cellHeight;
-	int spriteIdBegin, spriteIdMiddle, spriteIdEnd;
+protected:
 	int isBlocking;
 
-public: 
+public:
 	CColorBlock(float x, float y,
 		float cell_width, float cell_height, int length,
-		int sprite_id_begin, int sprite_id_middle, int sprite_id_end) : CGameObject(x, y)
+		int sprite_id_begin, int sprite_id_middle, int sprite_id_end) : CPlatform(x, y, cell_width, cell_height, length, sprite_id_begin, sprite_id_middle, sprite_id_end)
 	{
-		this->length = length;
-		this->cellWidth = cell_width;
-		this->cellHeight = cell_height;
-		this->spriteIdBegin = sprite_id_begin;
-		this->spriteIdMiddle = sprite_id_middle;
-		this->spriteIdEnd = sprite_id_end;
 		this->isBlocking = 0;
 	}
 
@@ -32,4 +22,3 @@ public:
 	int IsBlocking() { return this->isBlocking; }
 	void SetIsBlocking(int value) { this->isBlocking = value; };
 };
-
