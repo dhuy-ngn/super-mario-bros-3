d78f163b570293b125e96fc3d5d8aaa543f3313c
dhuy-ngn <dhuy.ngn@gmail.com>
2023-07-07 15:59:34 +0700

    modified parse method for brick

--- super-mario-bros-3/PlayScene.cpp
+++ super-mario-bros-3/PlayScene.cpp
@@ -151,7 +151,12 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 		break;
 	}
 
-	case OBJECT_TYPE_BRICK: obj = new CBrick(x, y); break;
+	case OBJECT_TYPE_BRICK: 
+	{
+		int item = atoi(tokens[3].c_str());
+		obj = new CBrick(x, y, item);
+		break;
+	}
 	case OBJECT_TYPE_COIN: obj = new CCoin(x, y); break;
 
 	case OBJECT_TYPE_PLATFORM:
