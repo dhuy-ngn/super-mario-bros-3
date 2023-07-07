1378a227a4f55da0d7b2809ffec269a9a0373cd9
dhuy-ngn <dhuy.ngn@gmail.com>
2023-07-07 14:07:20 +0700

    modified CBrick class

--- super-mario-bros-3/Brick.cpp
+++ super-mario-bros-3/Brick.cpp
@@ -1,4 +1,5 @@
 #include "Brick.h"
+#include "Mushroom.h"
 
 void CBrick::Render()
 {
@@ -7,10 +8,44 @@ void CBrick::Render()
 	//RenderBoundingBox();
 }
 
+void CBrick::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	y += vy * dt;
+
+	if (state == BRICK_STATE_EXHAUSTED)
+	{
+		if (GetTickCount64() - deflect_start >= BRICK_DEFLECT_DURATION)
+		{
+			vy = BRICK_DEFLECT_SPEED;
+		}
+		if (GetTickCount64() - deflect_end >= BRICK_DEFLECT_DURATION && y >= start_y)
+		{
+			y = start_y;
+			vy = 0;
+		}
+	}
+
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
 void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
 {
 	l = x - BRICK_BBOX_WIDTH/2;
 	t = y - BRICK_BBOX_HEIGHT/2;
 	r = l + BRICK_BBOX_WIDTH;
 	b = t + BRICK_BBOX_HEIGHT;
+}
+
+void CBrick::SetState(int state)
+{
+	switch (state) {
+	case BRICK_STATE_EXHAUSTED:
+		deflect_start = GetTickCount64();
+		deflect_end = deflect_start + BRICK_DEFLECT_DURATION;
+		vy = -BRICK_DEFLECT_SPEED;
+		break;
+	}
+
+	CGameObject::SetState(state);
 }
--- super-mario-bros-3/Brick.h
+++ super-mario-bros-3/Brick.h
@@ -9,10 +9,31 @@
 #define BRICK_BBOX_WIDTH 16
 #define BRICK_BBOX_HEIGHT 16
 
+#define BRICK_ITEM_TYPE_MUSHROOM	1
+#define BRICK_ITEM_TYPE_SWITCH		2
+#define BRICK_STATE_EXHAUSTED	1000
+
+#define	BRICK_DEFLECT_SPEED 0.1f
+#define BRICK_DEFLECT_DURATION 25
+
 class CBrick : public CGameObject {
+protected:
+	int item;
+	ULONGLONG deflect_start = 0;
+	ULONGLONG deflect_end = 0;
+
+	float start_y;
 public:
-	CBrick(float x, float y) : CGameObject(x, y) {}
+	CBrick(float x, float y, int item = 0) : CGameObject(x, y)
+	{
+		this->start_y = y;
+		this->item = item;
+	}
 	void Render();
-	void Update(DWORD dt) {}
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
+	int IsBlocking() { return 1; }
+	void SetState(int state);
+
+	int GetItemType() { return this->item; }
 };
